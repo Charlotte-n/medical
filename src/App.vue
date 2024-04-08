ce504f919f984f492e868034225a8d6ef9e91062
尹孟佳 <3495314473@qq.com>
2024-04-08 15:56:15 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
 onLaunch(() => {
-  console.log("App Launch");
-});
+    console.log('App Launch')
+})
 onShow(() => {
-  console.log("App Show");
-});
+    console.log('App Show')
+})
 onHide(() => {
-  console.log("App Hide");
-});
+    console.log('App Hide')
+})
 </script>
+<template>
+    <view>测试</view>
+</template>
 <style lang="scss">
-@import "@/uni_modules/uview-plus/theme.scss";
+@import '@/uni_modules/uview-plus/theme.scss';
 </style>
